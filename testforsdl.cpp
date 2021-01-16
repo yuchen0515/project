ba7bc9fa6aa4ddb4a993ed3a0bdc009adeb7e8a4
yuchen0515 <i98565412@gmail.com>
2021-01-16 17:13:57 +0800

    [介面] surface全面改texture成功

--- testforsdl.cpp
+++ testforsdl.cpp
@@ -14,14 +14,21 @@ SDL_Surface *loadSurface(char *str);
 bool init();
 void close();
 bool loadMedia(SDL_Surface **gHelloWorld, char *str);
+bool loadMedia(SDL_Texture **gTexture, char *str);
 void setup_bmp_size(SDL_Rect *dest, int32_t x, int32_t y, int32_t w, int32_t h);
+SDL_Texture *loadTexture(char *str);
 
 
 SDL_Window *window = NULL;
 SDL_Surface *screenSurface = NULL;
 SDL_Surface *screenBoard= NULL;
-SDL_Surface *screenBackground= NULL;
+
+//The window renderer
 SDL_Renderer *gRenderer = NULL;
+
+//current dusokated textyre
+SDL_Texture *gTextureBoard = NULL;
+SDL_Texture *gTextureBackground= NULL;
 
 
 
@@ -35,32 +42,29 @@ int main(){
     else
     {
         //Get window surface
-        screenSurface = SDL_GetWindowSurface(window);
-        //Fill the surface green
-        SDL_FillRect(screenSurface, NULL, SDL_MapRGBA(screenSurface->format, 255, 255, 255, 255));
-        SDL_UpdateWindowSurface(window);
+        
+        //screenSurface = SDL_GetWindowSurface(window);
+        ////Fill the surface green
+        //SDL_FillRect(screenSurface, NULL, SDL_MapRGBA(screenSurface->format, 255, 255, 255, 255));
+        //SDL_UpdateWindowSurface(window);
 
-        loadMedia(&screenBackground, (char *)"background.bmp");
+        //loadMedia(&screenBackground, (char *)"background.bmp");
 
         SDL_Rect dest;
-        setup_bmp_size(&dest, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-        SDL_BlitScaled(screenBackground, NULL, screenSurface, &dest);
-        SDL_UpdateWindowSurface(window);
-
-
+        loadMedia(&gTextureBackground, (char *)"background.bmp");
+        //SDL_BlitScaled(screenBackground, NULL, screenSurface, &dest);
+        //SDL_UpdateWindowSurface(window);
 
         //Load media
-        if( !loadMedia(&screenBoard, (char *)"board.bmp") )
+        if( !loadMedia(&gTextureBoard, (char *)"board.bmp") )
         {
             printf( "Failed to load media!\n" );
         }
         else
         {
-            //Apply the image
-            setup_bmp_size(&dest, 160, 140, 480, 960);
 
-            SDL_BlitScaled(screenBoard, NULL, screenSurface, &dest);
+            //SDL_BlitScaled(screenBoard, NULL, screenSurface, &dest);
             //Update the surface
             SDL_UpdateWindowSurface(window);
             //Wait two seconds
@@ -70,20 +74,30 @@ int main(){
             while (!quit){
                 while (SDL_PollEvent(&e)){
 
-
                     if (e.type == SDL_QUIT)
                         quit = true;
 
                     if (e.key.keysym.sym == SDLK_ESCAPE)
                         quit = true;
 
-                    //if (e.type == SDL_MOUSEBUTTONDOWN){
-                    //    quit = true;
-                    //}
+                    //Clear screen
+                    SDL_RenderClear( gRenderer );
+
+                    //Render texture to screen
+                    setup_bmp_size(&dest, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+                    SDL_RenderCopy(gRenderer, gTextureBackground, NULL, &dest);
+
+                    //Apply the image
+                    setup_bmp_size(&dest, 160, 140, 480, 960);
+                    SDL_RenderCopy(gRenderer, gTextureBoard, NULL, &dest);
+
+                    //Update screen
+                    SDL_RenderPresent( gRenderer );
                 }
             }
         }
     }
+
 
     close();
 
@@ -100,13 +114,19 @@ void setup_bmp_size(SDL_Rect *dest, int32_t x, int32_t y, int32_t w, int32_t h){
 
 void close()
 {
+    //Free loaded image
+    SDL_DestroyTexture( gTextureBoard );
+    gTextureBoard = NULL;
+
     //Deallocate surface
     SDL_FreeSurface(screenBoard);
     screenBoard = NULL;
 
     //Destroy window
+    SDL_DestroyRenderer( gRenderer );
     SDL_DestroyWindow(window);
     window = NULL;
+    gRenderer = NULL;
 
     //Quit SDL subsystems
     SDL_Quit();
@@ -130,6 +150,24 @@ bool loadMedia(SDL_Surface **gHelloWorld, char *str)
     return success;
 }
 
+bool loadMedia(SDL_Texture **gTexture, char *str)
+{
+    //Loading success flag
+    bool success = true;
+
+    //Load splash image
+    *gTexture= loadTexture(str);
+
+    if (*gTexture == NULL )
+    {
+        printf("Unable to load image %s!\n", str);
+        printf("SDL Error: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    return success;
+}
+
 bool init(){
     //The window we'll be rendering to
     //The surface contained by the window
@@ -139,6 +177,14 @@ bool init(){
         printf("SDL_Error detail: %s\n", SDL_GetError());
         return 0;
     }
+
+    //Set texture filtering to linear
+    if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+    {
+        printf( "Warning: Linear texture filtering not enabled!" );
+    }
+
+
 
     //Create window
     window = \
@@ -155,32 +201,79 @@ bool init(){
         printf("SDL_Error: %s\n", SDL_GetError());
         return 0;
     }
+
+    //Create renderer for window
+    gRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED );
+    if( gRenderer == NULL )
+    {
+        printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+        return 0;
+    }
+    //Initialize renderer color
+    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+    ////Initialize PNG loading
+    //int imgFlags = IMG_INIT_PNG;
+    //if( !( IMG_Init( imgFlags ) & imgFlags ) )
+    //{
+    //    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+    //    success = false;
+    //}
+
     return 1;
 }
 
 SDL_Surface* loadSurface( char *str )
 {
-	//The final optimized image
-	SDL_Surface* optimizedSurface = NULL;
+    //The final optimized image
+    SDL_Surface* optimizedSurface = NULL;
 
-	//Load image at specified path
-	SDL_Surface* loadedSurface = SDL_LoadBMP(str);
-	if( loadedSurface == NULL )
-	{
-		printf( "Unable to load image %s! SDL Error: %s\n", str, SDL_GetError() );
-	}
-	else
-	{
-		//Convert surface to screen format
-		optimizedSurface = SDL_ConvertSurface(loadedSurface, screenSurface->format, 0 );
-		if( optimizedSurface == NULL )
-		{
-			printf( "Unable to optimize image %s! SDL Error: %s\n", str, SDL_GetError() );
-		}
+    //Load image at specified path
+    SDL_Surface* loadedSurface = SDL_LoadBMP(str);
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL Error: %s\n", str, SDL_GetError() );
+    }
+    else
+    {
+        //Convert surface to screen format
+        optimizedSurface = SDL_ConvertSurface(loadedSurface, screenSurface->format, 0 );
+        if( optimizedSurface == NULL )
+        {
+            printf( "Unable to optimize image %s! SDL Error: %s\n", str, SDL_GetError() );
+        }
 
-		//Get rid of old loaded surface
-		SDL_FreeSurface( loadedSurface );
-	}
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
 
-	return optimizedSurface;
+    return optimizedSurface;
+}
+
+SDL_Texture *loadTexture(char *str)
+{
+    //The final texture
+    SDL_Texture* newTexture = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = SDL_LoadBMP(str);
+
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", str, SDL_GetError());
+    }
+    else
+    {
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+        if( newTexture == NULL )
+        {
+            printf( "Unable to create texture from %s! SDL Error: %s\n", str, SDL_GetError() );
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    return newTexture;
 }
