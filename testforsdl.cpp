0de65a44f66e34930a3b34679894bb5dfe8adc86
yuchen0515 <i98565412@gmail.com>
2021-01-16 14:45:12 +0800

    [按鍵] 新增esc跳出遊戲

--- testforsdl.cpp
+++ testforsdl.cpp
@@ -68,15 +68,17 @@ int main(){
             bool quit = false;
             while (!quit){
                 while (SDL_PollEvent(&e)){
-                    if (e.type == SDL_QUIT){
+
+
+                    if (e.type == SDL_QUIT)
                         quit = true;
-                    }
-                    //if (e.type == SDL_KEYDOWN){
+
+                    if (e.key.keysym.sym == SDLK_ESCAPE)
+                        quit = true;
+
+                    //if (e.type == SDL_MOUSEBUTTONDOWN){
                     //    quit = true;
                     //}
-                    if (e.type == SDL_MOUSEBUTTONDOWN){
-                        quit = true;
-                    }
                 }
             }
         }
