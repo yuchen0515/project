de3d4ac0be16d53dc76a949fb23730014fef8556
yuchen0515 <i98565412@gmail.com>
2021-04-19 10:32:51 +0800

    [mcts] Deal with the multiDEF for Move & class state declared problem

--- code/algorithm/mcts.h
+++ code/algorithm/mcts.h
@@ -1,14 +1,26 @@
 #ifndef MCTS_H_INCLUDED
 #define MCTS_H_INCLUDED
 
-#include "game.h"
+#include "state.h"
+
+#ifndef MOVE
+#define MOVE
+
+typedef struct move {
+    std::pair<int32_t, int32_t> from;
+    std::pair<int32_t, int32_t> to;
+}Move;
+
+#endif
+
+class State;
 
 namespace PURE {
-	Move MCTS(State state, int32_t simLimit);
+    Move MCTS(State state, int32_t simLimit);
 }
 
 namespace MCTSS {
-	Move MCTS(State state, int32_t simLimit);
+    Move MCTS(State state, int32_t simLimit);
 }
 
 #endif
