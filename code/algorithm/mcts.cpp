114716a2a99ff94d229f5ab00b0ccb5eda3a5aee
yuchen0515 <i98565412@gmail.com>
2021-05-11 10:28:10 +0800

    [mcts] Fixed, Memory Leak & Expand

--- code/algorithm/mcts.cpp
+++ code/algorithm/mcts.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <utility>
 #include <iomanip>
+#include <limits>
 #include <string>
 //#include <stdlib.h>
 //#include <omp.h>
@@ -39,7 +40,9 @@ namespace PURE {
             void print(std::ostream& os) const;
 
         private:
-            int32_t V, N;
+            double V;
+            int32_t N;
+
             Node* const parent;
             const Move move;
             const std::vector<Move> moves;
@@ -47,9 +50,10 @@ namespace PURE {
 
             Node* Select(State& state);
             Node* Expansion(State& state);
-            int32_t Simulate(State& state) const;
-            void Update(int32_t value);
+            double Simulate(State& state) const;
+            void Update(double value);
             void OneRound(State state);
+            bool Expanded() const;
     };
 
     Node::Node(const State& state)
@@ -76,17 +80,19 @@ namespace PURE {
 
     Node* Node::Select(State& state) {
         auto node = this;
-        while (node->children.size() == node->moves.size()) {
+        //while (node->children.size() == node->moves.size()) {
+        while (node->Expanded()) {
             //test
             //
             //std::cout << "turns: " << state.get_turns() << std::endl;
             //
-            double best_value = -2.0;
+            //double best_value = -2.0;
+            double best_value = -std::numeric_limits<double>::max();
             Node* best_child = nullptr;
             for (auto child : node->children) {
                 double uct =
-                    static_cast<double> (child->V) / child->N \
-                    + sqrt(2.0 * log(static_cast<double> (node->N)) / child->N);
+                    static_cast<double> (child->V) / (child->N + 1) \
+                    + sqrt(2.0 * log(static_cast<double> (node->N)) / (child->N + 1));
                     //static_cast<double> (child->V) / child->N \
                     //+ sqrt(2.0 * log(static_cast<double> (node->N)) / child->N);
 
@@ -96,6 +102,7 @@ namespace PURE {
                     best_child = child;
                 }
             }
+            assert(best_child);
             if (best_child == nullptr) {
                 break;
             }
@@ -112,26 +119,33 @@ namespace PURE {
     Node* Node::Expansion(State& state) {
         std::mt19937_64 rng(std::chrono::system_clock::now().time_since_epoch().count());
         auto node = this;
-        auto it = children.size();
+        //auto it = children.size();
         //auto it = rng() % moves.size();
         //std::cout << "hihiihihihihihihi" << std::endl;
         //std::cout << it << std::endl;
         //std::cout << moves.size() << std::endl;
         
-        auto move = moves[it];
-        state.do_Move(move);
         //
-        //state.switch_turn();
-        //
-        node = new Node(state, node, move);
-        children.emplace_back(node);
+        //auto move = moves[it];
+        //state.do_Move(move);
+        //node = new Node(state, node, move);
+        //children.emplace_back(node);
+        
+        for (size_t i = 0 ; i < moves.size(); ++i) {
+            children.emplace_back(new Node(state, this, moves[i]));
+        }
 
         return node;
     }
 
-    int32_t Node::Simulate(State& state) const {
+    double Node::Simulate(State& state) const {
         auto player_to_move = state.get_turns();
-        int32_t sim_dep = 50;
+        int32_t sim_dep = 7;
+        //
+        //sim_dep = 10;
+        if (player_to_move == 1) { // Lower
+            sim_dep = 7;
+        }
         int32_t best_move = 0;
 
         std::mt19937_64 rng(std::chrono::system_clock::now().time_since_epoch().count());
@@ -149,13 +163,18 @@ namespace PURE {
             //
             sim_dep--;
             //}
-            if (sim_dep <= 0){
-                return 0;
+            if (int32_t myself = (state.get_turns() == 0 ? 0 : 1);
+                    sim_dep <= 0){
+                //std::cerr << "myself " << myself << ": " << state.sigmoid(state.get_board_score(myself) - state.get_board_score(1 - myself)) << std::endl;
+                return state.sigmoid(state.get_board_score(myself) - state.get_board_score(1 - myself));
             }
             //std::cout << "Hello kity" << std::endl;
             //std::cout << move_num << std::endl;
             //std::cout << r << std::endl;
-    }
+            ////////
+            //
+            //std::cout << "Player: " << state.player_to_move_ << std::endl;
+        }
     if (state.is_Draw() == true) {
         return 0;
     }
@@ -166,106 +185,114 @@ namespace PURE {
     //return -30000;
     return -1;
     //return -2;
-}
-
-void Node::Update(int32_t value) {
-    auto node = this;
-    while (node != nullptr) {
-        node->V += value;
-        //node->V += node->move.value;
-        node->N += 1;
-        value = -value;
-        node = node->parent;
-    }
-}
-
-void Node::OneRound(State state) {
-    auto node = this;
-    node = node->Select(state);
-
-    if (state.is_End() == false) {
-        node = node->Expansion(state);
-    }
-    int32_t result = 0;
-    if (state.is_End() == true) {
-        //std::cout << "ya" << std::endl;
-        //if (state.is_Draw() == false) {
-        result = 1;
-        //result = 30000;
-        //}
-    } else {
-        result = node->Simulate(state);
     }
 
-    node->Update(result);
-    //node->Update(node->move.value);
-    //node->Update(node->move.value + result * 3000);
-}
-
-Move Node::Round(const State& state, int32_t simLimit) {
-    //int sim_cnt = 0;
-    /*//time limit
-      __int64 rtime;
-
-      rtime = clock() + simLimit *  CLOCKS_PER_SEC;
-      while (clock() < rtime){
-      OneRound(state);
-    //sim_cnt++;
-    }
-    */
-    //number limit
-    while (simLimit--) {
-        OneRound(state);
-    }
-
-    //std::cout << "sim_cnt=" << sim_cnt << endl;
-    auto best_move = State::no_move;
-    int32_t best_value = -1;
-    //int32_t best_value = -30000;
-    for (auto child : children) {
-        int32_t num = child->visit_num();
-        if (num > best_value) {
-            best_value = num;
-            best_move = child->get_move();
+    void Node::Update(double value) {
+        auto node = this;
+        while (node != nullptr) {
+            node->V += value;
+            //node->V += node->move.value;
+            node->N += 1;
+            value = -value;
+            node = node->parent;
         }
     }
-    return best_move;
-}
 
-std::string Node::detail() const {
-    double rate = mean() / 2 + 0.5;
-    std::stringstream ss;
-    ss << "Move from: ( " << move.from.first << ", " << move.from.second << " | ";
-    ss << "Move to: ( " << move.to.first << ", " << move.to.second << " | ";
-
-    //ss << "Move:" << static_cast<char>(move % 15 + 65);
-    //ss << getw(2) << (15 - move / 15);
-    ss << "\t Value:" << V;
-    ss << "\t Visited:" << N;
-    ss << "\t win-rate:" << rate;
-    return ss.str();
-}
-
-void Node::print(std::ostream& os) const {
-    for (auto child : children) {
-        os << child->detail() << std::endl;
+    bool Node::Expanded() const {
+        return !(children.empty());
     }
-}
 
-std::ostream& operator<<(std::ostream& os, const Node& node) {
+    void Node::OneRound(State state) {
+        auto node = this;
+        node = node->Select(state);
+
+        //if (state.is_End() == false) {
+        if (state.is_End() == false && node->Expanded() == false) {
+            node = node->Expansion(state);
+        }
+        double result = 0.0;
+        if (state.is_End() == true) {
+            //std::cout << "ya" << std::endl;
+            //if (state.is_Draw() == false) {
+            result = 1.0;
+            //result = 30000;
+            //}
+        } else {
+            result = node->Simulate(state);
+        }
+
+        //node->Update(result);
+        node->Update((result + state.sigmoid(node->move.value) * 2) / 3);
+        //node->Update(node->move.value + result * 3000);
+    }
+
+    Move Node::Round(const State& state, int32_t simLimit) {
+        //int sim_cnt = 0;
+        /*//time limit
+          __int64 rtime;
+
+          rtime = clock() + simLimit *  CLOCKS_PER_SEC;
+          while (clock() < rtime){
+          OneRound(state);
+        //sim_cnt++;
+        }
+        */
+        //number limit
+        while (simLimit--) {
+            OneRound(state);
+        }
+
+        //std::cout << "sim_cnt=" << sim_cnt << endl;
+        auto best_move = State::no_move;
+
+        int32_t best_value = -std::numeric_limits<int32_t>::max();
+        //int32_t best_value = -1;
+        //int32_t best_value = -30000;
+        for (auto child : children) {
+            int32_t num = child->visit_num();
+            if (num > best_value) {
+                best_value = num;
+                best_move = child->get_move();
+            }
+        }
+        return best_move;
+    }
+
+    std::string Node::detail() const {
+        double rate = mean() / 2 + 0.5;
+        std::stringstream ss;
+        ss << "Move from: ( " << move.from.first << ", " << move.from.second << " )" << " | ";
+        ss << "Move to: ( " << move.to.first << ", " << move.to.second << " )" << " | ";
+
+        //ss << "Move:" << static_cast<char>(move % 15 + 65);
+        //ss << getw(2) << (15 - move / 15);
+        ss << "\t Value:" << V;
+        ss << "\t Visited:" << N;
+        ss << "\t ch:" << children.size();
+        ss << "\t win-rate:" << rate;
+        return ss.str();
+    }
+
+    void Node::print(std::ostream& os) const {
+        for (auto child : children) {
+            os << child->detail() << std::endl;
+        }
+    }
+
+    std::ostream& operator<<(std::ostream& os, const Node& node) {
 #ifdef PRINT
-    node.print(os);
+        node.print(os);
 #endif
-    return os;
-}
+        return os;
+    }
 
-Move MCTS(State state, int32_t simLimit) {
-    Node root = Node(state);
-    Move move = root.Round(state, simLimit);
+    Move MCTS(State state, int32_t simLimit) {
+        Node root = Node(state);
+        Move move = root.Round(state, simLimit);
 #ifdef PRINT
-    std::cout << root << std::endl;
+        std::cout << root << std::endl;
 #endif
-    return move;
-}
+        return move;
+    }
 
-}  // namespace PURE
+    }  // namespace PURE
